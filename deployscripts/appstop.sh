8229a3643ea23d324392cca516858e111e5fcb19
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2020-06-04 18:21:27 +0530

    deploy script integration

--- deployscripts/appstop.sh
+++ deployscripts/appstop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/apps/auto_pilot
-JAVA_HOME=/usr/local/java/jdk1.7.0_95
+export JAVA_HOME=/usr/local/java/jdk1.7.0_95
 /usr/apache-ant-1.6.5/bin/ant stop_ap
 export PID=`ps -ef | grep java | grep auto_pilot | awk -F" " {'print $2'}`
 echo $PID
--- deployscripts/expand-app.sh
+++ deployscripts/expand-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/apps/auto_pilot
-JAVA_HOME=/usr/local/java/jdk1.7.0_95
+export JAVA_HOME=/usr/local/java/jdk1.7.0_95
 /usr/apache-ant-1.6.5/bin/ant deploy
